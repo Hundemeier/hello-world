3e9e5f183255a41a0779aed26d56bc0ba6aa9bc3
Hundemeier <hundemeier99@gmail.com>
2018-09-12 13:26:55 +0200

    added external dependencies

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 func main() {
+	color.Set(color.BgBlue)
 	fmt.Println("Hello world. This is a test")
+	color.Set(color.BgBlack)
 }
--- main_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestTest(t *testing.T) {
-	t.Error("this is a test error")
-}
